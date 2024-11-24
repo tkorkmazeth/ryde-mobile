fffc6183eae3c37b4dbce1cc585d65afde2abff7
Muhammed Talha Korkmaz <kriptovekediler@gmail.com>
2024-11-24 19:24:43 +0300

    config

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = (api) => {
+  api.cache(true);
+  return {
+    presets: [
+      [
+        "babel-preset-expo",
+        {
+          jsxImportSource: "nativewind",
+        },
+      ],
+      "nativewind/babel",
+    ],
+    plugins: [
+      // Required for expo-router
+      "@babel/plugin-proposal-export-namespace-from",
+      "react-native-reanimated/plugin",
+    ],
+  };
+};
